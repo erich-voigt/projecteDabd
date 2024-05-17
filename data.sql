92328620870fecb8e2cc3a15426b824c16123d39
Joaquin-Touron-UPC <150667837+Joaquin-Touron-UPC@users.noreply.github.com>
2024-05-17 22:54:06 +0200

    updated setup scripts

--- data.sql
+++ data.sql
@@ -1,113 +1,92 @@
 CREATE TYPE tipo_ejercicio AS ENUM ('Repeticiones', 'Cronometrado', 'Cardio');
 
-CREATE TABLE IF NOT EXISTS Usuario (
+CREATE TABLE IF NOT EXISTS usuario (
 	email VARCHAR(255) PRIMARY KEY NOT NULL,
 	contrasenya VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Plantilla_ejercicio (
+CREATE TABLE IF NOT EXISTS plantilla_ejercicio (
 	id UUID PRIMARY KEY NOT NULL,
 	nombre VARCHAR(255) NOT NULL,
 	instrucciones VARCHAR(2048) NOT NULL,
 	tipo tipo_ejercicio NOT NULL,
-	usuario VARCHAR(255) NOT NULL,
-	FOREIGN KEY (usuario) REFERENCES Usuario(email)
+	usuario VARCHAR(255) NOT NULL REFERENCES usuario(email)
 );
 
-CREATE TABLE IF NOT EXISTS Lista_inicial_plantillas_ejercicios (
+CREATE TABLE IF NOT EXISTS lista_inicial_plantilla_ejercicio (
 	id UUID PRIMARY KEY NOT NULL,
 	nombre VARCHAR(255) NOT NULL,
 	instrucciones VARCHAR(2048) NOT NULL,
 	tipo tipo_ejercicio NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Entrenamiento (
+CREATE TABLE IF NOT EXISTS entrenamiento (
 	id UUID PRIMARY KEY NOT NULL,
 	fecha_inicio DATE NOT NULL,
-	fecha_fin DATE NOT NULL
+	fecha_fin DATE NOT NULL,
+	usuario VARCHAR(255) NOT NULL REFERENCES usuario(email)
 );
 
-CREATE TABLE IF NOT EXISTS Entrenamiento_plantilla (
-	entrenamiento UUID NOT NULL,
-	plantilla UUID NOT NULL,
-	PRIMARY KEY (entrenamiento, plantilla),
-	FOREIGN KEY (plantilla) REFERENCES Plantilla_ejercicio(id),
-	FOREIGN KEY (entrenamiento) REFERENCES Entrenamiento(id)
-);
-
-CREATE TABLE IF NOT EXISTS Grupo_muscular (
+CREATE TABLE IF NOT EXISTS grupo_muscular (
 	nombre VARCHAR(255) PRIMARY KEY NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Grupo_plantilla (
-	grupo VARCHAR(255) NOT NULL,
-	plantilla UUID NOT NULL,
-	PRIMARY KEY (grupo, plantilla),
-	FOREIGN KEY (plantilla) REFERENCES Plantilla_ejercicio(id),
-	FOREIGN KEY (grupo) REFERENCES Grupo_muscular(nombre)
+CREATE TABLE IF NOT EXISTS grupo_plantilla (
+	grupo VARCHAR(255) NOT NULL REFERENCES grupo_muscular(nombre),
+	plantilla UUID NOT NULL REFERENCES plantilla_ejercicio(id),
+	PRIMARY KEY (grupo, plantilla)
 );
 
-CREATE TABLE IF NOT EXISTS Grupo_lista_inicial (
-	grupo VARCHAR(255) NOT NULL,
-	plantilla UUID NOT NULL,
-	PRIMARY KEY (grupo, plantilla),
-	FOREIGN KEY (plantilla) REFERENCES Lista_inicial_plantillas_ejercicios(id),
-	FOREIGN KEY (grupo) REFERENCES Grupo_muscular(nombre)
+CREATE TABLE IF NOT EXISTS grupo_lista_inicial (
+	grupo VARCHAR(255) NOT NULL REFERENCES grupo_muscular(nombre),
+	plantilla UUID NOT NULL REFERENCES lista_inicial_plantilla_ejercicio(id),
+	PRIMARY KEY (grupo, plantilla)
 );
 
-CREATE TABLE IF NOT EXISTS Equipamiento (
+CREATE TABLE IF NOT EXISTS equipamiento (
 	nombre VARCHAR(255) PRIMARY KEY NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Equipamiento_plantilla (
-	equipamiento VARCHAR(255) NOT NULL,
-	plantilla UUID NOT NULL,
-	PRIMARY KEY (equipamiento, plantilla),
-	FOREIGN KEY (plantilla) REFERENCES Plantilla_ejercicio(id),
-	FOREIGN KEY (equipamiento) REFERENCES Equipamiento(nombre)
+CREATE TABLE IF NOT EXISTS equipamiento_plantilla (
+	equipamiento VARCHAR(255) NOT NULL REFERENCES equipamiento(nombre),
+	plantilla UUID NOT NULL REFERENCES plantilla_ejercicio(id),
+	PRIMARY KEY (equipamiento, plantilla)
 );
 
-CREATE TABLE IF NOT EXISTS Equipamiento_lista_inicial (
-	equipamiento VARCHAR(255) NOT NULL,
-	plantilla UUID NOT NULL,
-	PRIMARY KEY (equipamiento, plantilla),
-	FOREIGN KEY (plantilla) REFERENCES Lista_inicial_plantillas_ejercicios(id),
-	FOREIGN KEY (equipamiento) REFERENCES Equipamiento(nombre)
+CREATE TABLE IF NOT EXISTS equipamiento_lista_inicial (
+	equipamiento VARCHAR(255) NOT NULL REFERENCES equipamiento(nombre),
+	plantilla UUID NOT NULL REFERENCES lista_inicial_plantilla_ejercicio(id),
+	PRIMARY KEY (equipamiento, plantilla)
 );
 
-CREATE TABLE IF NOT EXISTS Ejercicio_en_curso (
+CREATE TABLE IF NOT EXISTS ejercicio_en_curso (
 	id UUID UNIQUE NOT NULL,
 	finalizado BOOLEAN NOT NULL DEFAULT FALSE,
-	entrenamiento UUID NOT NULL,
-	plantilla UUID NOT NULL,
-	PRIMARY KEY (entrenamiento, plantilla),
-	FOREIGN KEY (plantilla) REFERENCES Plantilla_ejercicio(id),
-	FOREIGN KEY (entrenamiento) REFERENCES Entrenamiento(id)
+	entrenamiento UUID NOT NULL REFERENCES entrenamiento(id),
+	plantilla UUID NOT NULL REFERENCES plantilla_ejercicio(id),
+	PRIMARY KEY (entrenamiento, plantilla)
 );
 
-CREATE TABLE IF NOT EXISTS Serie (
+CREATE TABLE IF NOT EXISTS serie (
 	id UUID PRIMARY KEY NOT NULL,
-	finalizado BOOLEAN NOT NULL DEFAULT FALSE,
-	FOREIGN KEY (id) REFERENCES Ejercicio_en_curso(id)
+	finalizada BOOLEAN NOT NULL DEFAULT FALSE,
+	ejercicio UUID NOT NULL REFERENCES ejercicio_en_curso(id)
 );
 
-CREATE TABLE IF NOT EXISTS Repeticiones (
-	serie_id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS repeticiones (
+	serie UUID PRIMARY KEY NOT NULL REFERENCES serie(id),
 	peso INT NOT NULL,
-	repeticiones INT NOT NULL,
-	FOREIGN KEY (serie_id) REFERENCES Serie(id)
+	repeticiones INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Cronometrado (
-	serie_id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS cronometrado (
+	serie UUID PRIMARY KEY NOT NULL REFERENCES serie(id),
 	peso INT NOT NULL,
-	tiempo INT NOT NULL,
-	FOREIGN KEY (serie_id) REFERENCES Serie(id)
+	tiempo INT NOT NULL 
 );
 
-CREATE TABLE IF NOT EXISTS Cardio (
-	serie_id UUID PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS cardio (
+	serie UUID PRIMARY KEY NOT NULL REFERENCES serie(id),
 	distancia INT NOT NULL,
-	tiempo INT NOT NULL,
-	FOREIGN KEY (serie_id) REFERENCES Serie(id)
+	tiempo INT NOT NULL 
 );
--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 tables = [
-	"Usuario",
-	"Plantilla_ejercicio",
-	"Lista_inicial_plantillas_ejercicios",
-	"Entrenamiento",
-	"Entrenamiento_plantilla",
-	"Grupo_muscular",
-	"Grupo_plantilla",
-	"Grupo_lista_inicial",
-	"Equipamiento",
-	"Equipamiento_plantilla",
-	"Equipamiento_lista_inicial",
-	"Ejercicio_en_curso",
-	"Serie",
-	"Repeticiones",
-	"Cronometrado",
-	"Cardio",
+	"usuario",
+	"plantilla_ejercicio",
+	"lista_inicial_plantilla_ejercicio",
+	"entrenamiento",
+	"grupo_muscular",
+	"grupo_plantilla",
+	"grupo_lista_inicial",
+	"equipamiento",
+	"equipamiento_plantilla",
+	"equipamiento_lista_inicial",
+	"ejercicio_en_curso",
+	"serie",
+	"repeticiones",
+	"cronometrado",
+	"cardio"
 ]
 
 enums = ["tipo_ejercicio"]
 
 def drop_everything(conn, cur):
 	global tables, enums
-	cur.execute("SET search_path TO practica;")
+	cur.execute("SET search_path TO practica")
 	for table in tables[::-1]:
 		cur.execute(f"DROP TABLE IF EXISTS {table}")
 	for enum in enums:
@@ -29,7 +28,7 @@ def drop_everything(conn, cur):
 	conn.commit()
 
 def create_everything(conn, cur):
-	cur.execute("SET search_path TO practica;")
+	cur.execute("SET search_path TO practica")
 	with open("data.sql", "r") as file:
 		cur.execute(file.read())
 	conn.commit()
