bd011f5b4281f58097d39a8933eacc4622d7b503
Erich <erich.voigt@estudiantat.upc.edu>
2026-01-20 04:48:10 +0100

    api core with connection to db

---
+++ javaBackend/ezfit/src/main/java/vooigt/dev/ezfit/EzfitApplication.java
@@ -0,0 +1,13 @@
+package vooigt.dev.ezfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzfitApplication.class, args);
+	}
+
+}
